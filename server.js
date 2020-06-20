d05ade1a82a5b738cce9561154af51dbb0673e88
Rachel Lee <bokyounglee@berkeley.edu>
2020-06-20 16:16:21 -0700

    change port

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static(__dirname + '/dist/'));
 app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
